0288e6e1ebb24d06118b5456176e7e8d0e453c42
arcan1s <darkarcanis@mail.ru>
2015-01-18 04:16:40 +0300

    edit translations build

--- sources/translations/desktop-panel/extract_messages.sh
+++ sources/translations/desktop-panel/extract_messages.sh
@@ -1,40 +1,48 @@
 #!/bin/sh
-BASEDIR="../" # root of translatable sources
+# root of translatable sources
+BASEDIR="../../"
+PATHS="desktop-panel"
 PROJECT="plasma_applet_desktop-panel" # project name
 BUGADDR="https://github.com/arcan1s/awesome-widgets/issues" # MSGID-Bugs
-WDIR=`pwd` # working dir
+# working dir
+WDIR=`pwd`
+
 
 echo "Preparing rc files"
-cd "${BASEDIR}"
-find . -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' -o -name '*.py' | sort > "${WDIR}/rcfiles.list"
-xargs --arg-file=${WDIR}/rcfiles.list extractrc > "${WDIR}/rc.cpp"
-echo 'i18nc("NAME OF TRANSLATORS","Your names");' >> "${WDIR}/rc.cpp"
-echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> "${WDIR}/rc.cpp"
+cd ${BASEDIR}
+# we use simple sorting to make sure the lines do not jump around too much from system to system
+find . -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' | grep ${PATHS} | sort > ${WDIR}/rcfiles.list
+xargs --arg-file=${WDIR}/rcfiles.list extractrc > ${WDIR}/rc.cpp
+# additional string for KAboutData
+echo 'i18nc("NAME OF TRANSLATORS","Your names");' >> ${WDIR}/rc.cpp
+echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> ${WDIR}/rc.cpp
+cd ${WDIR}
 echo "Done preparing rc files"
 
 echo "Extracting messages"
-cd "${WDIR}"
-cd "${BASEDIR}"
-find . -name '*.cpp' -o -name '*.h' -o -name '*.c' -o -name '*.py' | sort > "${WDIR}/infiles.list"
-echo "rc.cpp" >> "${WDIR}/infiles.list"
-
-cd "${WDIR}"
+cd ${BASEDIR}
+# see above on sorting
+find . -name '*.cpp' -o -name '*.h' -o -name '*.qml' | grep ${PATHS} | sort > ${WDIR}/infiles.list
+echo "rc.cpp" >> ${WDIR}/infiles.list
+cd ${WDIR}
 xgettext --from-code=UTF-8 -C -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
---msgid-bugs-address="${BUGADDR}" --language=Python \
---files-from=infiles.list -D "${BASEDIR}" -D "${WDIR}" -o "${PROJECT}.pot" || { echo "error while calling xgettext. aborting."; exit 1; }
+--msgid-bugs-address="${BUGADDR}" \
+--files-from=infiles.list -D ${BASEDIR} -D ${WDIR} -o ${PROJECT}.pot || { echo "Error while calling xgettext, aborting."; exit 1; }
 echo "Done extracting messages"
 
 echo "Merging translations"
-catalogs=$(find . -name '*.po')
+catalogs=`find . -name '*.po'`
 for cat in $catalogs; do
-  echo $cat
-  msgmerge -o "$cat.new" "$cat" "${PROJECT}.pot"
-  mv "$cat.new" "$cat"
+    echo $cat
+    msgmerge -o $cat.new $cat ${PROJECT}.pot
+    mv $cat.new $cat
 done
 echo "Done merging translations"
 
+
 echo "Cleaning up"
-cd "${WDIR}"
+cd ${WDIR}
 rm -f rcfiles.list infiles.list rc.cpp
 echo "Done"
+
