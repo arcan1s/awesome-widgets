4dbf5150fcc03e9ed15a5eb15ca548576b583bee
arcan1s <darkarcanis@mail.ru>
2015-02-11 07:38:30 +0300

    update supporting files

--- create_archive.sh
+++ create_archive.sh
@@ -6,8 +6,11 @@ MINOR=$(grep -m1 PROJECT_VERSION_MINOR sources/CMakeLists.txt | awk '{print $3}'
 PATCH=$(grep -m1 PROJECT_VERSION_PATCH sources/CMakeLists.txt | awk '{print $3}' | sed 's/.$//g')
 VERSION="${MAJOR}.${MINOR}.${PATCH}"
 
+# update submodules
+git submodule update --init --recursive
+
 # build dataengine
-ARCHIVE="ext-sysmon"
+ARCHIVE="extsysmon"
 # create archive
 [[ -e ${ARCHIVE}-${VERSION}-src.tar.xz ]] && rm -f ${ARCHIVE}-${VERSION}-src.tar.xz
 [[ -d ${ARCHIVE} ]] && rm -rf "${ARCHIVE}"
@@ -18,7 +21,7 @@ rm -rf "${ARCHIVE}"
 # build widget
 ARCHIVE="awesome-widgets"
 FILES="AUTHORS CHANGELOG CHANGELOG-RU COPYING"
-IGNORELIST="build usr"
+IGNORELIST="build usr .kdev4 sources.kdev4"
 # create archive
 [[ -e ${ARCHIVE}-${VERSION}-src.tar.xz ]] && rm -f "${ARCHIVE}-${VERSION}-src.tar.xz"
 [[ -d ${ARCHIVE} ]] && rm -rf "${ARCHIVE}"
@@ -26,9 +29,13 @@ cp -r "${SRCDIR}" "${ARCHIVE}"
 for FILE in ${FILES[*]}; do cp -r "$FILE" "${ARCHIVE}"; done
 for FILE in ${IGNORELIST[*]}; do rm -rf "${ARCHIVE}/${FILE}"; done
 tar cJf "${ARCHIVE}-${VERSION}-src.tar.xz" "${ARCHIVE}"
+ln -sf "../${ARCHIVE}-${VERSION}-src.tar.xz" arch
 rm -rf "${ARCHIVE}"
 
 # update md5sum
 MD5SUMS=$(md5sum ${ARCHIVE}-${VERSION}-src.tar.xz | awk '{print $1}')
-sed -i "/md5sums=('[0-9A-Fa-f]*/s/[^'][^)]*/md5sums=('${MD5SUMS}'/" PKGBUILD
-sed -i "s/pkgver=[0-9.]*/pkgver=${VERSION}/" PKGBUILD
+sed -i "/md5sums=('[0-9A-Fa-f]*/s/[^'][^)]*/md5sums=('${MD5SUMS}'/" arch/PKGBUILD{-kf5,-kde4}
+sed -i "s/pkgver=[0-9.]*/pkgver=${VERSION}/" arch/PKGBUILD{-kf5,-kde4}
+# clear
+find . -type f -name '*src.tar.xz' -not -name "*${VERSION}-src.tar.xz" -exec rm -rf {} \;
+find arch -type l -xtype l -exec rm -rf {} \;
