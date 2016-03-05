be9203e816ffb8f81e295f1029c0569e782d080d
arcan1s <i@arcanis.me>
2016-03-05 19:39:26 +0300

    update project url
    For some time my site has been moved from arcanis.name to arcanis.me.
    Also https protocol forced.

--- sources/packages-recipe.cmake
+++ sources/packages-recipe.cmake
@@ -21,7 +21,7 @@ set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CP
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "plasma-framework")
 set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Collection of minimalistic Plasmoids which look like Awesome WM widgets (ex-PyTextMonitor)")
 set(CPACK_DEBIAN_PACKAGE_ENHANCES "mpd, smartmontools")
-set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "https://arcanis.name/projects/awesome-widgets")
+set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "https://arcanis.me/projects/awesome-widgets")
 set(CPACK_DEBIAN_PACKAGE_SECTION "KDE")
 
 # rpm specific
@@ -30,7 +30,7 @@ set(CPACK_RPM_PACKAGE_GROUP "User Interface/Desktops")
 set(CPACK_RPM_PACKAGE_LICENSE "${PROJECT_LICENSE}")
 set(CPACK_RPM_PACKAGE_REQUIRES "plasma-framework")
 set(CPACK_RPM_PACKAGE_SUGGESTS "mpd, smartmontools")
-set(CPACK_RPM_PACKAGE_URL "https://arcanis.name/projects/awesome-widgets")
+set(CPACK_RPM_PACKAGE_URL "https://arcanis.me/projects/awesome-widgets")
 
 if (BUILD_DEB_PACKAGE OR BUILD_RPM_PACKAGE)
     include(CPack)
