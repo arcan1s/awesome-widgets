f6a6704fd24a5d101fba1ff3904c025e3e8e53ac
arcan1s <i@arcanis.me>
2016-06-23 10:56:55 +0300

    add test for awupdatehelper and template for awkeycache

---
+++ sources/test/testawupdatehelper.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *   This file is part of awesome-widgets                                  *
+ *                                                                         *
+ *   awesome-widgets is free software: you can redistribute it and/or      *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation, either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   awesome-widgets is distributed in the hope that it will be useful,    *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with awesome-widgets. If not, see http://www.gnu.org/licenses/  *
+ ***************************************************************************/
+
+
+#ifndef TESTAWUPDATEHELPER_H
+#define TESTAWUPDATEHELPER_H
+
+#include <QObject>
+
+
+class AWUpdateHelper;
+
+class TestAWUpdateHelper : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    // initialization
+    void initTestCase();
+    void cleanupTestCase();
+    // test
+    void test_checkVersion();
+
+private:
+    AWUpdateHelper *plugin = nullptr;
+};
+
+
+#endif /* TESTAWUPDATEHELPER_H */
